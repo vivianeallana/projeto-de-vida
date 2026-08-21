b8348ddd06780b953f4c4de176ccb89c545f14bc
vivianeallana <allana.viviane.luz@escola.pr.gov.br>
2026-08-21 08:14:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-
+const botoes = document.quertSelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+  
+for(let i=o;i <botoes.lenght;i++){
+  botoes[i}.onclick = function(){
+    
+for(let j=0;j<botoes.lenght;j++){
+  botoes[j].classList.remove("ativo");
+  textos[j].classList.remove("ativo");
+}
+    
+    botoes[i}.classList.add("ativo");
+    textos{i}.classList.remove("ativo");
+}
+  }
